03b53af35764af341736e74805a5d6b54c8f90ef
s-ude <ude@mediatis.de>
2024-05-05 21:41:20 +0200

    [TASK] Rework Configuration Structure (MFR-74) (#2)

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,11 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Damian Ickler',
     'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
-    'version' => '1.0.0',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-12.4.99',
+            'dmf_core' => '2.0.0-2.99.99',
         ],
         'conflicts' => [
         ],
