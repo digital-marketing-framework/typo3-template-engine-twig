44a4ffd40dd364d913692011b67ed15e469ee16b
s-ude <ude@mediatis.de>
2025-05-25 22:16:52 +0200

    [TASK] TYPO3 13 Update, Backend Module Handling (MFR-136) (#4)

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,17 +1,17 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Digital Marketing Framework - Template Engine - Twig',
+    'title' => 'Anyrel - Template Engine - Twig',
     'description' => 'Use Twig Template Engine',
     'category' => 'be',
     'author' => 'Damian Ickler',
     'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
-    'version' => '2.1.0',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
-            'dmf_core' => '2.0.0-2.99.99',
+            'typo3' => '12.4.0-13.4.99',
+            'dmf_core' => '3.0.0-3.99.99',
         ],
         'conflicts' => [
         ],
