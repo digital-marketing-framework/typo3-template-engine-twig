60963f853e620562485e991dd0dab186773a7c0d
Stephan Ude <ude@mediatis.de>
2026-05-26 16:57:44 +0200

    [BUGFIX] Dependency Fixes

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,11 +7,11 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Damian Ickler',
     'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
-    'version' => '4.0.0',
+    'version' => '4.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
-            'dmf_core' => '3.0.0-3.99.99',
+            'typo3' => '12.4.0-14.99.99',
+            'dmf_core' => '4.0.0-4.99.99',
         ],
         'conflicts' => [
         ],
